fad17685f27f826180a070b0a874c5b9f33f38b8
Omar Rashwan <omaralpha1@gmail.com>
2020-02-04 11:59:48 +0200

    LastEdit

---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public class DContext : DbContext
+    {
+        public DContext(DbContextOptions<DContext> options) : base (options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
