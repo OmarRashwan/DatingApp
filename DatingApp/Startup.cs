e1531b480ae3d8c53d0ac06e8a35a9cb9ad48200
Omar Rashwan <omaralpha1@gmail.com>
2020-02-26 11:54:29 +0200

    ErrorHandlerDONE

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -14,7 +14,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
-
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DatingApp.Helpers;
 namespace DatingApp
 {
     public class Startup
@@ -54,8 +57,22 @@ namespace DatingApp
                 {
                     app.UseDeveloperExceptionPage();
                 }
+                else
+            {
+                app.UseExceptionHandler(builder => { builder.Run(async context => {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
+                    if (error != null)
+                    {
+                        context.Response.AddApplicationError(error.Error.Message);
+                        await context.Response.WriteAsync(error.Error.Message);
+                        
+                    }
+                });
+                    });
 
-                app.UseRouting();
+            }
+            app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
 
