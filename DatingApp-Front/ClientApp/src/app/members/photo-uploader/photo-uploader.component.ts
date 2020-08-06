076b6cd1cc4c0a4646dfc3c10606442b738814f5
Omar Rashwan <omaralpha1@gmail.com>
2020-08-06 11:39:14 +0200

    Photos

--- DatingApp-Front/ClientApp/src/app/members/photo-uploader/photo-uploader.component.ts
+++ DatingApp-Front/ClientApp/src/app/members/photo-uploader/photo-uploader.component.ts
@@ -36,5 +36,18 @@ export class PhotoUploaderComponent implements OnInit {
       autoUpload: false,
       maxFileSize: 10 * 1024 * 1024
     });
-  }
+
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      const res: photo = JSON.parse(response);
+      const photo = {
+        id: res.id,
+        url: res.url,
+        dateAdded: res.dateAdded,
+        description: res.description,
+        isMain: res.isMain
+      };
+      this.photos.push(photo);
+    }
+  };
 }
